3e640e76173d5759be99a2d88db64774f28bc5e6
Susan Cox <susancox@Susans-Laptop.local>
2024-07-24 07:41:17 -0500

    Task 1 complete

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -8,5 +8,14 @@ describe("Command class", function() {
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
-
+  it("constructor sets command type", function() {
+    const command = new Command("MOVE", 30);
+    expect(command.commandType).toEqual("MOVE");
+  });
+  it("constructor sets a value passed in as 2nd argument", function() {
+    const command = new Command("MOVE", 30);
+    expect(command.value).toEqual(30);
+  });
 });
+
+
